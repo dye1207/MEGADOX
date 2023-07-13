cf7a19b66a6fa80403da6690808eb40633cbe4ce
dye1207 <kdoyoung1207@gmail.com>
2023-07-13 16:29:44 +0900

    영화평점 사이트 생성

--- src/App.js
+++ src/App.js
@@ -1,29 +1,52 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  
-  function increase(){
-    setCount(count +1)
-    // setCount((prevValue) => (prevValue + 1))
-  }
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
 
-  function decrease(){
-    setCount((prevValue) => (prevValue - 1))
-  }
+  useEffect(() => {
+    async function getMovies() {
+      try {
+        const movies = await axios.get(
+          "https://yts.mx/api/v2/list_movies.json?sort_by=rating"
+        );
+        setMovies(movies.data.data.movies); //20개의 배열 데이터
+        setIsLoading(false);
+      } catch (error) {
+        console.log("오류");
+      }
+    }
+    getMovies();
+  }, []); // 빈배열은 처음에만 수행
 
   return (
-    <div>
-        <h1>리엑트 복습</h1>
-        <h3>함수 기반 Hooks 연습</h3>
-        <hr />
-        <h3>
-          현재 숫자의 값: <span style={{fontSize: "30px"}}>{count}</span>
-        </h3>
-        <button onClick={increase}>증가</button>
-        <button onClick={decrease}>감소</button>
-    </div>
+    <section className="container">
+      {isLoading ? (
+        <div className="loader">
+          <span className="loader__text">Loading...</span>
+        </div>
+      ) : (
+        <div className="movies">
+          {movies.map((movie) => {
+            return (
+              <Movie
+                // key={movie.id} 유일한 값이라 key로 설정
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            );
+          })}
+        </div>
+      )}
+    </section>
   );
 }
-
 export default App;
